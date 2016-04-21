fe2880a70b02b13583c53d67fde549db8fb515da
Jinhao Ping <pingjinhao@gmail.com>
2016-04-21 13:01:14 -0400

    test3
    test3

--- main.cpp
+++ main.cpp
@@ -454,7 +454,19 @@ bool searchMatrix(vector<vector<int>>& matrix, int target) {
     return TRUE;
 
 }
- 
+
+int integerBreak(int n) {
+    if(n == 2 )  return 1;
+    if(n == 3 )  return 2;
+    if(n == 4)  return 4;
+    if(n == 5 )  return 6;
+    if(n == 6 )  return 9;
+    return 3*integerBreak(n-3);
+
+
+
+   
+    }
 int main(){
 
     cout << "test " ;
