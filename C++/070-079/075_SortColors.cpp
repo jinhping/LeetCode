04c9ce853c3d1ad609786305b9e8152c12f82ad9
Jinhao Ping <pingjinhao@gmail.com>
2016-11-05 16:38:01 -0500

    more qs

---
+++ C++/070-079/075_SortColors.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int second = nums.size() - 1;
+        int zero = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            while (nums[i] == 2 && i < second) {
+                swap(nums[i], nums[second]);
+                second--;
+            }
+            while (nums[i] == 0 && i > zero) {
+                swap(nums[i], nums[zero]);
+                zero++;
+            }
+        }
+    }
+};
