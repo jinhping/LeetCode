29ba09e1b9d3ecadaf8bc123af1d67cea9db08b7
Jinhao Ping <pingjinhao@gmail.com>
2016-12-24 01:59:38 -0600

    house robber updates

--- C++/190-199/198_HouseRobber.cpp
+++ C++/190-199/198_HouseRobber.cpp
@@ -1,20 +1,15 @@
+//滚动数组
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.size() == 0) {
-            return 0;
+        int pre = 0;
+        int cur = 0;
+        int n = nums.size();
+        for(int i = 0; i < n ; i++){
+            int tmp = max(pre + nums[i], cur);
+            pre = cur;
+            cur = tmp;
         }
-        if (nums.size() == 1) {
-            return nums[0];
-        }
-        int rob = nums[0];
-        int unrob = 0;
-        vector<int> res(nums.size());
-        res[0] = rob;
-        res[1] = max(nums[1], nums[0]);
-        for (int i = 2; i < nums.size(); i++) {
-           res[i] = max(res[i - 1], res[i - 2] + nums[i]); 
-        }
-        return res[nums.size() - 1];
+        return cur;
     }
 };
