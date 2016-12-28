b27cf5e12169741603f0c6a42da8aa71a6323f78
Jinhao Ping <pingjinhao@gmail.com>
2016-12-27 20:03:09 -0600

    find peak element solution 2

--- C++/160-169/162_FindPeakElement.cpp
+++ C++/160-169/162_FindPeakElement.cpp
@@ -1,3 +1,4 @@
+//Solution 1
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
@@ -22,6 +23,32 @@ public:
             return low;
         } else {
             return high;
+        }
+    }
+};
+
+
+//Solution 2
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid;
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        if (nums[start] > nums[end]) {
+            return start;
+        } else {
+            return end;
         }
     }
 };
