03ff4287ba52fcb20f948f36c61af01877942241
Jinhao Ping <pingjinhao@gmail.com>
2016-12-31 01:06:28 -0600

    median of two sorted arrays

---
+++ C++/000-009/004_MedianOfTwoSortedArrays.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    double helper(vector<int>& nums1, int m, vector<int>& nums2, int n, int k, int start1, int start2){
+        if(m > n)
+            return helper(nums2, n, nums1, m, k, start2, start1);
+        if(m == 0)
+            return nums2[k-1];
+        if(k == 1)
+            return min(nums1[start1], nums2[start2]);
+        int a = min(k/2, m);
+        int b = k-a;
+        if(nums1[a+start1-1] <= nums2[b+start2-1]){
+            return helper(nums1, m-a, nums2, n, k-a, start1+a, start2);
+        }
+        else{
+            return helper(nums1, m, nums2, n-b, k-b, start1, start2+b);
+        }
+    }
+    
+    double findMedianSortedArrays(vector<int> A, vector<int> B) {
+        vector<int> nums1 = A;
+        vector<int> nums2 = B;
+        int m = nums1.size(), n = nums2.size();
+        int total = m+n;
+        if(total%2 == 1)
+            return helper(nums1, m, nums2, n, total/2+1, 0, 0);
+        else
+            return (helper(nums1, m, nums2, n, total/2, 0, 0) + helper(nums1, m, nums2, n, total/2+1, 0, 0))/2;
+    }
+};
