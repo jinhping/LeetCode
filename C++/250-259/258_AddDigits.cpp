755ef1eb2aba84c811c78937d45b4c6e300a8791
Jinhao Ping <pingjinhao@gmail.com>
2016-12-31 14:56:09 -0600

    add digits

--- C++/250-259/258_AddDigits.cpp
+++ C++/250-259/258_AddDigits.cpp
@@ -1,6 +1,26 @@
+//Solution 1
 class Solution {
 public:
     int addDigits(int num) {
        return (num - 1)%9 + 1;
+    }
+};
+
+
+//Solution 2
+class Solution {
+public:
+    int addDigits(int num) {
+        if (num < 10 ) return num;
+        int result = 0;
+        while( num >= 10 ){
+            result = num%10 + result;
+            num = num/10;
+        }
+        result = result + num;
+        while(result >= 10){
+            return  addDigits(result);
+        }
+        return result;
     }
 };
