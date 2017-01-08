3afaca12bffbcf1dfef2ba9a03e6c14000310edd
Jinhao Ping <pingjinhao@gmail.com>
2017-01-07 20:36:18 -0600

    3 Sum

--- C++/010-019/015_3Sum.cpp
+++ C++/010-019/015_3Sum.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
-    void helper (vector<vector<int>> &results, vector<int> &nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
+    //sort
+    //first,
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> results;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++){
+            if ((i > 0) && (nums[i] == nums[i-1])) continue;
             int j = i + 1;
             int k = nums.size() - 1;
             while (j < k) {
-                
                 if (nums[i] + nums[j] + nums[k] == 0) {
-                    vector<int> tmp;
-                    tmp.push_back(nums[i]);
-                    tmp.push_back(nums[j]);
-                    tmp.push_back(nums[k]);
+                    vector<int> tmp = {nums[i], nums[j], nums[k]};
                     results.push_back(tmp);
-                    while (j < k && nums[j + 1] == nums[j]) {
-                        j++;
+                    while (nums[j] == nums[j + 1]) {
+                         j++;
                     }
-                    while (j < k && nums[k - 1] == nums[k]) {
+                    while (nums[k] == nums[k - 1]) {
                         k--;
                     }
                     j++;
                     k--;
-                }   
+                }
                 else if (nums[i] + nums[j] + nums[k] > 0) {
                     k--;
-                } 
+                }
                 else if (nums[i] + nums[j] + nums[k] < 0) {
                     j++;
                 }
-                
             }
         }
-    }
-
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> results;
-        helper(results, nums);
         return results;
     }
 };
